63fd832f8971c4bc98bcd5604f806521e7297343
Bryan Nkala <nkala.bryan3@gmail.com>
2024-10-08 14:40:29 +0200

    changed the server stop script

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -2,17 +2,22 @@
 
 #APP_NAME = "backend"
 
-PID=$(pgrep -f "backend")
+#PID=$(pgrep -f "backend")
 
-if [ -z "$PID" ]; then
-  echo "Nest.js application is not running."
-else
-  echo "Stopping Nest.js application with PID: $PID"
-  kill -9 $PID
+#if [ -z "$PID" ]; then
+#  echo "Nest.js application is not running."
+#else
+#  echo "Stopping Nest.js application with PID: $PID"
+#  kill -9 $PID
 
-  if [ $? -eq 0 ]; then
-    echo "Nest.js application stopped successfully."
-  else 
-    echo "Failed to stop the Nest.js application."
-  fi
-fi 
+#  if [ $? -eq 0 ]; then
+#    echo "Nest.js application stopped successfully."
+#  else 
+#    echo "Failed to stop the Nest.js application."
+#  fi
+#fi 
+
+echo "Stopping Nest.js application..."
+pm2 stop backend
+pm2 delete backen
+echo "Application stopped."
